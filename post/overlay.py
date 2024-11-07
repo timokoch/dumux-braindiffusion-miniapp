4811b2d2117bdae8c71a0b9d0b7e43bcaaba0be4
Timo Koch <timokoch@uio.no>
2024-11-07 16:53:10 +0100

    Improve mapped result to MRI plot

--- post/overlay.py
+++ post/overlay.py
@@ -4,15 +4,8 @@ import matplotlib.pyplot as plt
 import click
 from pathlib import Path
 
-def overlay_images(overlay: Path, background: Path, ax, slicer_op, min_val=None, max_val=None):
-    # Load your two NIfTI images
-    background_nii = nib.load(background)
-    overlay_nii = nib.load(overlay)
 
-    # Extract the image data
-    background_data = background_nii.get_fdata()
-    overlay_data = overlay_nii.get_fdata()
-
+def overlay_images(overlay_data: np.ndarray, background_data: np.ndarray, ax, slicer_op, min_val=None, max_val=None):
     # Choose a specific slice index (for example, slice along the z-axis)
     background_slice = slicer_op(background_data)
     overlay_slice = slicer_op(overlay_data)
@@ -32,12 +25,26 @@ def overlay_images(overlay: Path, background: Path, ax, slicer_op, min_val=None,
     ax.axis("off")
 
 
+def niifile_to_ndarray(nii: Path) -> np.ndarray:
+    return nib.load(nii).get_fdata()
+
+
+def overlay_image_files(overlay: Path, background: Path, ax, slicer_op, min_val=None, max_val=None):
+    # Load the two NIfTI images and extract the image data
+    overlay_images(
+        background_data=niifile_to_ndarray(background),
+        overlay_data=niifile_to_ndarray(overlay),
+        ax=ax, slicer_op=slicer_op,
+        min_val=min_val, max_val=max_val,
+    )
+
+
 @click.command()
 @click.option("--overlay", type=Path, required=True)
 @click.option("--background", type=Path, required=True)
 def plot_data(overlay: Path, background: Path):
     fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-    overlay_images(overlay, background, ax)
+    overlay_image_files(overlay, background, ax)
     plt.show()
 
 if __name__ == "__main__":
--- post/plot_mapped_mri.py
+++ post/plot_mapped_mri.py
@@ -1,27 +1,43 @@
 import matplotlib.pyplot as plt
-from overlay import overlay_images
+import matplotlib as mpl
+from overlay import overlay_images, niifile_to_ndarray
 from map_to_mri import TIME_STAMPS, VTK_FIELDS, PREFIX
 import numpy as np
 
+mpl.rcParams['savefig.pad_inches'] = 0
+
 if __name__ == '__main__':
-    fig, axs = plt.subplots(2, len(TIME_STAMPS), figsize=(25, 10))
+    fig, axs = plt.subplots(2, len(TIME_STAMPS), figsize=(25, 6), frameon=False)
+    background_data = niifile_to_ndarray('../data/sub-01_ses-01_T1w_registered.nii.gz')
     for j in range(len(VTK_FIELDS)):
         for i, time_stamp in enumerate(TIME_STAMPS):
             overlay_images(
-                f'{PREFIX}{VTK_FIELDS[j]}_{time_stamp}.nii.gz',
-                '../data/sub-01_ses-01_T1w_registered.nii.gz',
-                axs[j][i],
-                #lambda img: img[:, :, 310].T,
-                lambda img: np.flipud(img[150, :, :].T),
+                overlay_data=niifile_to_ndarray(f'{PREFIX}{VTK_FIELDS[j]}_{time_stamp}.nii.gz'),
+                background_data=background_data,
+                ax=axs[j][i],
+                #slicer_op=lambda img: img[:, :, 310].T,
+                slicer_op=lambda img: np.flipud(img[150, :, :].T),
                 min_val=0, max_val=0.05,
             )
             axs[j][i].annotate(
                 f'{time_stamp/86400*24:.0f}h',
                 xy=(0.04, 0.96), xycoords='axes fraction',
-                color='white', fontsize=11,
+                color='white', fontsize=16,
                 ha='left', va='top',
             )
 
+            axs[j][i].margins(0)
+            axs[j][i].get_xaxis().set_visible(False)
+            axs[j][i].get_yaxis().set_visible(False)
+
+    plt.autoscale(tight=True)
     plt.tight_layout()
-    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.subplots_adjust(wspace=0, hspace=0, left=0.1, right=0.9, bottom=0.1, top=0.9)
+
+    # add vertical colorbar on the right spanning both rows
+    cbar_ax = fig.add_axes([0.9, 0.1, 0.02, 0.8])
+    cbar = fig.colorbar(axs[0][0].images[1], cax=cbar_ax)
+    cbar.ax.tick_params(labelsize=16)
+    cbar.set_label('c in mmol/l', fontsize=18)
+
     plt.show()

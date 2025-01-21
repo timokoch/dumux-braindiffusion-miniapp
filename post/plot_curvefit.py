385dbf66b4005a64cb02b1ad116a5f1db4cb3b40
Timo Koch <timokoch@uio.no>
2025-01-21 16:26:34 +0100

    [post] Fix curvefit plot script

--- post/plot_curvefit.py
+++ post/plot_curvefit.py
@@ -4,24 +4,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
+import click
 
-# Data for plotting
-t = np.genfromtxt("times.dat")
-pt = np.genfromtxt("plottimes.dat")
+@click.command()
+@click.option('--folder', default="../build-cmake/app", help='Folder containing the data')
+@click.option('-n', "--number-of-plots", default=2, help='Number of plots before stopping (integer)', type=int)
+def plot_curves(folder: str, number_of_plots: int):
+    t = np.genfromtxt(f"{folder}/times.dat")
+    pt = np.genfromtxt(f"{folder}/plottimes.dat")
 
-# glob all files with the pattern curvefit-*.dat
-files = sorted(glob.glob("curvefit-*.dat"))
-files_data = sorted(glob.glob("data-*.dat"))
+    # glob all files with the pattern curvefit-*.dat
+    files = sorted(glob.glob(f"{folder}/curvefit-*.dat"))
+    files_data = sorted(glob.glob(f"{folder}/data-*.dat"))
 
-batch_size = 100
-for i in range(0, len(files), batch_size):
-    fig, axs = plt.subplots(10, 10, figsize=(15, 9), sharex=True, sharey=True)
-    for j, (f, d) in enumerate(zip(files[i:i+batch_size], files_data[i:i+batch_size])):
-        data = np.genfromtxt(f)
-        data_data = np.genfromtxt(d)
-        ax = axs[j // 10, j % 10]
-        ax.plot(t/86400.0, data_data, 'ro', label='data')
-        ax.plot(pt/86400.0, data, 'b-', label='fit')
-    plt.tight_layout()
-    plt.subplots_adjust(wspace=0, hspace=0)
-    plt.show()
+    batch_size = 100
+    for i in range(0, len(files), batch_size):
+        if i >= number_of_plots*batch_size:
+            break
+
+        fig, axs = plt.subplots(10, 10, figsize=(15, 9), sharex=True, sharey=True)
+        for j, (f, d) in enumerate(zip(files[i:i+batch_size], files_data[i:i+batch_size])):
+            data = np.genfromtxt(f)
+            data_data = np.genfromtxt(d)
+            ax = axs[j // 10, j % 10]
+            ax.plot(t/86400.0, data_data, 'ro', label='data')
+            ax.plot(pt/86400.0, data, 'b-', label='fit')
+        fig.tight_layout()
+        fig.subplots_adjust(wspace=0, hspace=0)
+        plt.show()
+
+
+if __name__ == '__main__':
+    plot_curves()

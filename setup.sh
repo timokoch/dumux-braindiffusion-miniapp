efb0b2f81ce8c2df3e294e9942ec954611491021
Timo Koch <timokoch@uio.no>
2025-01-21 14:43:42 +0100

    [setup] Upgrade to dumux master

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ git clone -b releases/2.10 https://gitlab.dune-project.org/core/dune-grid.git
 git clone -b releases/2.10 https://gitlab.dune-project.org/core/dune-istl.git
 git clone -b releases/2.10 https://gitlab.dune-project.org/core/dune-localfunctions.git
 git clone -b releases/2.10 https://gitlab.dune-project.org/extensions/dune-alugrid.git
-git clone -b 3f1db4cb2c085433cc82bc68b062146285bc3c8e https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git
+git clone -b 1e239fcb1d7a11aae5cb76d58a08b700609cfdfb https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git
 
 # configure & build libs
 ./dune-common/bin/dunecontrol --opts=dumux-braindiffusion-miniapp/cmake.opts cmake

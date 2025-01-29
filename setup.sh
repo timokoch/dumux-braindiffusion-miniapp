cceba2dc9bde33c30f7844ea74cf634661e357fd
Timo Koch <timokoch@uio.no>
2025-01-29 22:03:16 +0100

    [setup] Check out submodule to get gridformat

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ git clone -b releases/2.10 https://gitlab.dune-project.org/core/dune-istl.git
 git clone -b releases/2.10 https://gitlab.dune-project.org/core/dune-localfunctions.git
 git clone -b releases/2.10 https://gitlab.dune-project.org/extensions/dune-alugrid.git
 git clone -b master https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git
+cd dumux && git submodule update --init && cd ..
 cd dumux && git reset --hard 1e239fcb1d7a11aae5cb76d58a08b700609cfdfb && cd ..
 
 # configure & build libs
